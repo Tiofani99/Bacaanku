031f56eee71a0a024ea4b679d3da453d4b65685c
rezafp01 <noblesvan@gmail.com>
2021-09-22 22:01:48 +0700

    test hehehe
    Signed-off-by: rezafp01 <noblesvan@gmail.com>

--- app/src/main/java/com/id/bacaanku/ui/main/HomeActivity.kt
+++ app/src/main/java/com/id/bacaanku/ui/main/HomeActivity.kt
@@ -2,11 +2,19 @@ package com.id.bacaanku.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.viewModels
 import com.id.bacaanku.R
 
 class HomeActivity : AppCompatActivity() {
+    private val viewModel: NewsViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        viewModel.headLineNews.observe(this,{
+            Log.d("TESTH", "onCreate: ${it}")
+        })
+
     }
 }
---
+++ app/src/main/java/com/id/bacaanku/ui/main/NewsViewModel.kt
@@ -0,0 +1,53 @@
+package com.id.bacaanku.ui.main
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.id.bacaanku.BuildConfig
+import com.id.bacaanku.data.remote.network.ApiConfig
+import com.id.bacaanku.data.remote.response.ArticlesItem
+import com.id.bacaanku.data.remote.response.NewsHeadlineResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class NewsViewModel : ViewModel() {
+    private val _headlineNews = MutableLiveData<List<ArticlesItem>>()
+    val headLineNews: LiveData<List<ArticlesItem>> get() = _headlineNews
+
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> get() = _isLoading
+
+    companion object{
+        private const val TAG = "NewsViewModel"
+    }
+
+    init{
+        getHeadlineNews()
+    }
+
+    private fun getHeadlineNews(){
+        _isLoading.value = true
+        val client = ApiConfig.getApiService().getEverything("kesehatan",BuildConfig.API_KEY)
+        client.enqueue(object : Callback<NewsHeadlineResponse> {
+            override fun onResponse(
+                call: Call<NewsHeadlineResponse>,
+                response: Response<NewsHeadlineResponse>
+            ) {
+                _isLoading.value = false
+                if(response.isSuccessful){
+                    _headlineNews.value = response.body()?.articles!!
+                }else{
+                    Log.e(TAG, "onResponse: ${response.message()}")
+                }
+            }
+
+            override fun onFailure(call: Call<NewsHeadlineResponse>, t: Throwable) {
+                _isLoading.value = false
+                Log.e(TAG, "onFailure: ${t.message.toString()}")
+            }
+
+        })
+    }
+}
