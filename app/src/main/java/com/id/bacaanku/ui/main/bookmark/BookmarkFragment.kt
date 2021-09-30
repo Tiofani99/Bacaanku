688f7522ab31fbd930e755abb1015218ed0205c3
rezafp01 <noblesvan@gmail.com>
2021-09-30 09:50:11 +0700

    update xml Bookmark

--- app/src/main/java/com/id/bacaanku/ui/main/bookmark/BookmarkFragment.kt
+++ app/src/main/java/com/id/bacaanku/ui/main/bookmark/BookmarkFragment.kt
@@ -5,15 +5,34 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.id.bacaanku.R
+import androidx.fragment.app.activityViewModels
+import com.id.bacaanku.databinding.FragmentBookmarkBinding
+import com.id.bacaanku.ui.main.NewsViewModel
 
 
 class BookmarkFragment : Fragment() {
+
+    private var position: Int? = 0
+    private var _binding: FragmentBookmarkBinding? = null
+    private val binding get() = _binding!!
+    private val viewModel: NewsViewModel by activityViewModels()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bookmark, container, false)
+        _binding = FragmentBookmarkBinding.inflate(layoutInflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
